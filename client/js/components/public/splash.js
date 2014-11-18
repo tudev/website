02af140ecc32f3241676d647915baf153842057f
Sandile Keswa <me@sandile.io>
2014-11-17 23:37:16 -0500

    more progress to overall look and feel

--- client/js/components/public/splash.js
+++ client/js/components/public/splash.js
@@ -1,18 +1,26 @@
 /** @jsx React.DOM */
-var React   = require('react');
+var React   = require('react'),
+    Router  = require('react-router');
+
+// React-router variables
+var Link    = Router.Link;
 
 var Splash = React.createClass({
     render: function() {
         var blurb = {
-            title: 'We\'re Temple Dev.',
-            subtitle: 'We write software and build things.'
+            title: 'We\'re Temple Dev',
+            subtitle: 'We write software and build things'
         };
 
         return (
             <div id="splash">
                 <div id="blurb">
                     <h1>{blurb.title}</h1>
+                    <div className="divider"/>
                     <h2>{blurb.subtitle}</h2>
+                    <div className="buttons">
+                        <Link to="about" id="learn-more">Learn More</Link>
+                    </div>
                 </div>
             </div>
         );
